50300493f33316a3604ce70c503481a409b16edc
sebastian-nb <sebastian.haas@grape.at>
2013-07-23 21:06:54 +0200

    Changed models Added dictionary

--- FertCalculator/models.py
+++ FertCalculator/models.py
@@ -4,45 +4,38 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Patient(models.Model):
+    user = models.OneToOneField(User)
+    height = models.SmallIntegerField()
+
+
 class Record(models.Model):
-    user = models.ForeignKey(User)
+    patient = models.ForeignKey(Patient)
     datetime = models.DateTimeField(auto_now=True)
-
-
-class Weight(models.Model):
     weight = models.PositiveSmallIntegerField()
-    record = models.ForeignKey(Record)
-
-
-class FollicleStimulatingHormone(models.Model):
-    fsh = models.DecimalField(max_digits=5, decimal_places=2)  # international units per litre, IU/L
-    record = models.ForeignKey(Record)
-
-
-class Estrogen(models.Model):
-    estrogen = models.SmallIntegerField()  # pg/mL
-    record = models.ForeignKey(Record)
-
-
-class Spermiogram(models.Model):
-    volume = models.PositiveIntegerField()  # microlitre, µ/L [10^-6 L]
-    ph = models.DecimalField(max_digits=3, decimal_places=1)
-    spermcount = models.PositiveIntegerField()  # millions per millilitre
-    totalspermcount = models.PositiveIntegerField()
-    motility = models.PositiveSmallIntegerField()  # percent
-    morphology = models.PositiveSmallIntegerField()  # percent
-    record = models.ForeignKey(Record)
-
-
-class MenstrualPeriod(models.Model):
-    regularity = models.PositiveSmallIntegerField()  # deviance in days
-    record = models.ForeignKey(Record)
-
-
-class AMH(models.Model):
     amh = models.PositiveSmallIntegerField()  # nanogram per millilitre, ng/mL
+    fsh = models.DecimalField(max_digits=5, decimal_places=2)  # international units per litre, IU/L
+    tsh = models.DecimalField(max_digits=2, decimal_places=1)  # micro-international units per millilitre
+    estrogen = models.SmallIntegerField()  # pg/mL
+    regularity = models.PositiveSmallIntegerField()  # deviance of the regular menstrual cycle length in days
+
+
+class Pregnancy(models.Model):
     record = models.ForeignKey(Record)
+    outcome = models.ForeignKey(PregnancyOutcome)
 
 
+class PregnancyOutcome(models.Model):
+    name = models.CharField
+
+
+# class Spermiogram(models.Model):
+#     volume = models.PositiveIntegerField()  # microlitre, µ/L [10^-6 L]
+#     ph = models.DecimalField(max_digits=3, decimal_places=1)
+#     spermcount = models.PositiveIntegerField()  # millions per millilitre
+#     totalspermcount = models.PositiveIntegerField()
+#     motility = models.PositiveSmallIntegerField()  # percent
+#     morphology = models.PositiveSmallIntegerField()  # percent
+#     record = models.ForeignKey(Record)
 
 
